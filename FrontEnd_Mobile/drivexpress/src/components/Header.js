92b61a81a794fddad2b8fa7f1fd41ae311615eb1
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-11-17 19:08:18 -0300

    Correção dos componentes Drawer e Header

--- FrontEnd_Mobile/drivexpress/src/components/Header.js
+++ FrontEnd_Mobile/drivexpress/src/components/Header.js
@@ -2,24 +2,25 @@ import React, { useRef, useState } from "react";
 import { DrawerLayoutAndroid, StyleSheet, View } from "react-native";
 import { Appbar, Drawer, Divider, Button } from "react-native-paper";
 
-
-
-
-const Header = ({ children }) => {
+const Header = ({ title, children }) => {
   const drawer = useRef(null);
   const [drawerPosition] = useState("left");
 
-
-
   const navigationView = () => (
     <View>
+      <View>
+        <Appbar.Header>
+          <Appbar.Content title={title} />
+        </Appbar.Header>
+      </View>
+
       <Drawer.Section style={styles.menu}>
         <View style={styles.espacamento} />
 
         <Drawer.Item
           icon="home"
           label="Home"
-          onPress={() => navigation.navigate("Home")}
+          onPress={() => navigation.navigate("HomePage")}
         />
         <Drawer.Item
           icon="#"
@@ -41,11 +42,11 @@ const Header = ({ children }) => {
           label="#"
           onPress={() => navigation.navigate("#")}
         />
-          <Drawer.Item
-            icon="#"
-            label="#"
-            onPress={() => navigation.navigate("#")}
-          />
+        <Drawer.Item
+          icon="#"
+          label="#"
+          onPress={() => navigation.navigate("#")}
+        />
         <Drawer.Item
           icon="#"
           label="#"
@@ -63,43 +64,43 @@ const Header = ({ children }) => {
         />
         <Divider />
         <Drawer.Item
-          icon="#"
+          icon="contacts-outline"
           label="Contatos"
-          onPress={() => navigation.navigate("TermosUso")}
+          onPress={() => navigation.navigate("Contatos")}
         />
         <Drawer.Item
-          icon="#"
+          icon="file-document-edit-outline"
           label="Termos de uso"
           onPress={() => navigation.navigate("Politica")}
         />
-
       </Drawer.Section>
     </View>
   );
 
   return (
-      <DrawerLayoutAndroid
-        ref={drawer}
-        drawerWidth={250}
-        drawerPosition={drawerPosition}
-        renderNavigationView={navigationView}
-      >
-        <View style={styles.container}>
-          <Appbar.Header style={styles.header}>
-            <Appbar.Action
-              icon="menu"
-              color="white"
-              onPress={() => drawer.current.openDrawer()}
-            />
-            <Button
-              icon="magnify"
-              textColor="white"
-              //onPress={() => navigation.navigate("#")}
-            />
-          </Appbar.Header>
-        </View>
-        {children}
-      </DrawerLayoutAndroid>
+    <DrawerLayoutAndroid
+      ref={drawer}
+      drawerWidth={250}
+      drawerPosition={drawerPosition}
+      renderNavigationView={navigationView}
+    >
+      <View style={styles.container}>
+        <Appbar.Header style={styles.header}>
+          <Appbar.Action
+            icon="menu"
+            color="white"
+            onPress={() => drawer.current.openDrawer()}
+          />
+          <Button
+            icon="magnify"
+            textColor="white"
+            //onPress={() => navigation.navigate("#")}
+          />
+        </Appbar.Header>
+      </View>
+
+      {children}
+    </DrawerLayoutAndroid>
   );
 };
 
